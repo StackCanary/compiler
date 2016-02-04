dfd7cb43821a5bc64abe66f01bf5f9b0335c9f03
A Zaidi <saaz@st-andrews.ac.uk>
2016-02-04 12:40:24 +0000

    This commit adds the contents of the template.

---
+++ logoCompiler/lexer/Lexer.java
@@ -0,0 +1,33 @@
+package logoCompiler.lexer;
+import java.util.*;
+
+
+public final class Lexer {
+  static int ch = ' ';
+
+  public static Token lex() {
+
+    //skip the white space
+    while (ch == ' ' || ch == '\n' || ch == '\t') {
+ 	ch = getChar();
+    }
+
+    //identify new character and return correct token
+    switch (ch) {
+    	case -1 : {
+	    return new EOIToken();
+    	}
+    }
+  }
+
+  
+  //this reads chars from stdin. You can read in files any way you want, using FileReader etc.
+  static int getChar() {
+    try {
+      ch = System.in.read();
+    } catch (Exception e) {
+      System.out.println(e); System.exit(1);
+    }
+    return ch;
+  }
+}
