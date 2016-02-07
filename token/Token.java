63e74cec76704862b7c7c65127012d32d5026b6b
A Zaidi <saaz@st-andrews.ac.uk>
2016-02-07 13:16:23 +0000

    Added more tokens and added them to be returned in the Tokenizer (the switch statement)

--- token/Token.java
+++ token/Token.java
@@ -1,11 +1,26 @@
 package token;
 public abstract class Token {
+	private String attr;
 	
+	public Token() {
+		
+	}
+	
+	public Token(String attr) {
+		this.attr = attr;
+	}
 	
 	public int precedence() {
 		//set precedence of all non-operators to 0
 		//Override this for Operators
 	    return 0;
 	  } 
+	
+	/*
+	 * Override this method for all tokens with attributes
+	 */
+	public String getAttr() {
+		return null;
+	}
 	
 }
