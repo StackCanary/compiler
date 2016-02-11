234e03d64c02c9ce931be9f79023801b0a59e574
A Zaidi <saaz@st-andrews.ac.uk>
2016-02-11 13:49:18 +0000

    Same as previous commit, removed useless functions

--- expr/PrimaryExpr.java
+++ expr/PrimaryExpr.java
@@ -2,6 +2,7 @@ package expr;
 import  logoCompiler.lexer.*;
 import logoCompiler.parser.Parser;
 import token.IdentToken;
+import token.NumberToken;
 /*
  * primary-expr:
  *   num
@@ -9,12 +10,14 @@ import token.IdentToken;
  */
 public abstract class PrimaryExpr extends Expr {
   public static Expr parse() {
-    if (Parser.t instanceof NumToken) {
+    if (Parser.t instanceof NumberToken) {
       return NumExpr.parse();
     } else if (Parser.t instanceof IdentToken) {
       return IdentExpr.parse();
     } else {
       //error?
     } 
+    
+    return null;
   }
 }
