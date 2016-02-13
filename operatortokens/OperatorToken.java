c432c5623cefca41271c32ff57a01960cd93a133
A Zaidi <saaz@st-andrews.ac.uk>
2016-02-13 13:53:46 +0000

    Fixed Precedence and PsOpCode for BinaryOperators. BinaryOperators extend and implement OperatorToken

--- operatortokens/OperatorToken.java
+++ operatortokens/OperatorToken.java
@@ -5,4 +5,8 @@ import token.Token;
 public abstract class OperatorToken extends Token {
 	public abstract String psOpCode();
 	public abstract int precedence();
+	
+	public OperatorToken(String attr) {
+		super(attr);
+	}
 }
